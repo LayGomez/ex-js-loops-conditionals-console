0d54bb03344214fb2001ae8ae2be27f8d75d1834
LayGomez <laylamelanygomez@gmail.com>
2024-10-07 15:56:18 +0200

    Exercise 4 y 5

--- condicionalsLoops.js
+++ condicionalsLoops.js
@@ -44,7 +44,19 @@ if (pet =="perro") {
 //Ejercicio 5: Haz el mismo ejercicio 3, pero con el condicional switch.
 
 //Escribe tu código aquí
-
+switch (pet) {
+    case "perro":
+        console.log("Tengo un perro")
+        break;
+    case "gato":
+        console.log("Tengo un gato")
+        break;
+    case "pajaro":
+        console.log("Tengo un pájaro")
+        break;
+    default:
+        console.log("No tengo una mascota convencional.") 
+}
 
 //Ejercicio 6: Usando un operador ternario, escribe un programa que lea la constante weather e imprima en consola si es soleado "Me vestiré con un vestido" y si no es así que imprima "Me vestiré con pantalón"
 
